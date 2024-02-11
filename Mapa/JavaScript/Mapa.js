b8fe6d5bb16ea20e4eb1e7e6a393273468f43b41
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2024-02-10 19:28:27 -0500

    Create Mapa.js

---
+++ Mapa/JavaScript/Mapa.js
@@ -0,0 +1,9 @@
+let map = L.map('map').setView([6.246540778104689, -75.56101422297017],5)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+document.getElementById('select-location').addEventListener('change',function(e){
+  let coords = e.target.value.split(",");
+  map.flyTo(coords,13);
+})
